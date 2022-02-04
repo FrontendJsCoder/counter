32411380e8eac34088654876ea7381eca003afdc
sizwe <nkosiczwe@gmail.com>
2022-02-04 23:33:38 +0200

    Add comments js file

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
+//DOM element find by HTML id
 let numContainer = document.getElementById('num')
+//Initialize value
 let value = 0;
-
+//DOM element find HTML elements by CSS selectors
 let btnInc = document.querySelector('.inc')
 let btnDec = document.querySelector('.dec')
 let btnReset = document.querySelector('.reset')
-
+//Event listener to increment (counter) value
 btnInc.addEventListener('click', () => {
     value++;
     numContainer.textContent = value;
 });
-
+//Event listener to decrement value
 btnDec.addEventListener('click', () => {
     value--;
     numContainer.textContent = value;
 });
-
+//Event listener to reset value
 btnReset.addEventListener('click', () => {
     value = 0;
     numContainer.textContent = value;
