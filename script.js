5abb051b14abaa41f2b3d118b9f3a0ae7edbf94e
sizwe <nkosiczwe@gmail.com>
2022-02-05 14:32:06 +0200

    Add pop-out menu functionality when button is clicked

--- script.js
+++ script.js
@@ -21,3 +21,24 @@ btnReset.addEventListener('click', () => {
     value = 0;
     numContainer.textContent = value;
 });
+
+let toggleBtn = document.getElementById('toggle-btn')
+let menuItems = document.querySelectorAll('.menu a')
+let menuActive = false;
+
+toggleBtn.addEventListener('click', () => {
+    if (!menuActive) {
+        menuItems[0].style.transform = 'translate(120px,0)';
+        menuItems[1].style.transform = 'translate(100px,55px)';
+        menuItems[2].style.transform = 'translate(55px,100px)';
+        menuItems[3].style.transform = 'translate(0,120px)';
+        menuActive = true;
+        toggleBtn.classList.add('active')
+    }else {
+        menuItems.forEach((menuItem) => {
+            menuItem.style.transform = 'translate(0.0)';
+        });
+        menuActive = false;
+        toggleBtn.classList.remove('active');
+    }
+});
